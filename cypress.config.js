492503183597c694449659e814278616ecbac736
Rafael Cardoso <comerciorafael@hotmail.com>
2023-02-17 17:48:22 -0300

    Update cypress.config.js
    Correção no Project ID.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'u5x39s',
+  projectId: 'cyz38p',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
